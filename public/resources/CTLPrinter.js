d76d234e5f03cd42ff41514af5022344cb02f84a
siraj <siraj.ea@padath.com>
2019-12-02 10:39:55 +0530

    completed print feature

--- public/resources/CTLPrinter.js
+++ public/resources/CTLPrinter.js
@@ -7,150 +7,153 @@ var d = today.getDate();
 var h = today.getHours();
 var m = today.getMinutes();
 var s = today.getSeconds();
-const TMP_DOC = './outputPDF/Slip_' + d + '_' + m + s + '.pdf';
+const TMP_DOC = './public/outputPDF/Slip_' + d + '_' + m + s + '.pdf';
 
 class CTLPrinter {
-    constructor(printerName, options = {}) {
-        if (!printerName) {
-            throw new Error('No printer selected');
-        }
+	constructor(printerName, options = {}) {
+		if (!printerName) {
+			throw new Error('No printer selected');
+		}
 
-        this.options = {};
-        this.defaults = {
-            file: TMP_DOC,
-            printerName,
-            width: 204.0,
-            height: 567.0,
-            margins: 0,
-            deleteGenerated: true,
-            fontSize: 36,
-            cutPage: false,
-            cutDocument: true
-        };
+		this.options = {};
+		this.defaults = {
+			file: TMP_DOC,
+			printerName,
+			width: 204.0,
+			height: 567.0,
+			margins: 0,
+			deleteGenerated: true,
+			fontSize: 36,
+			cutPage: false,
+			cutDocument: true
+		};
 
-        this.options = this._mergeOptions(this.defaults, options);
-    }
+		this.options = this._mergeOptions(this.defaults, options);
+	}
 
-    _mergeOptions(previous, options) {
-        return Object.assign({}, previous, options);
-    }
+	_mergeOptions(previous, options) {
+		return Object.assign({}, previous, options);
+	}
 
-    _createOutput(doc, _opts, end) {
-        const writeStream = fs.createWriteStream(_opts.file);
+	_createOutput(doc, _opts, end) {
+		const writeStream = fs.createWriteStream(_opts.file);
 
-        writeStream.on('finish', () => {
-            this._sendFile(_opts.file, _opts, end)
-        });
+		writeStream.on('finish', () => {
+			this._sendFile(_opts.file, _opts, end);
+		});
 
-        doc.pipe(writeStream);
-    }
+		doc.pipe(writeStream);
+	}
 
-    _sendFile(file, { cutPage, cutDocument, printerName }, end) {
-        const child = spawn('lp', [
-            '-d', printerName,
-            '-o', `PageCutType=${ cutPage ? '1PartialCutPage' : '0NoCutPage' }`,
-            '-o', `DocCutType=${ cutDocument ? '1PartialCutDoc' : '0NoCutDoc' }`,
-            file
-        ]);
+	_sendFile(file, { cutPage, cutDocument, printerName }, end) {
+		const child = spawn('lp', [
+			'-d',
+			printerName,
+			'-o',
+			`PageCutType=${cutPage ? '1PartialCutPage' : '0NoCutPage'}`,
+			'-o',
+			`DocCutType=${cutDocument ? '1PartialCutDoc' : '0NoCutDoc'}`,
+			file
+		]);
 
-        child.stdout.on('data', data => {
-            const msg = data.toString();
+		child.stdout.on('data', (data) => {
+			const msg = data.toString();
 
-            if (msg.indexOf('request id is') >= 0) {
-                this.log("\n" + "Print added. ID: " + msg.split(' ')[3]);
-            }
-        });
-        
-        child.stderr.on('data', data => {
-            this._displayMessage('There was an error printing:', data.toString());
-        });
+			if (msg.indexOf('request id is') >= 0) {
+				this.log('\n' + 'Print added. ID: ' + msg.split(' ')[3]);
+			}
+		});
 
-        child.on('exit', (code, signal) => {
-            if (signal) {
-                this.log(`Exited with code ${code} and signal ${signal}`);
-            } else {
-                this._displayMessage(false, 'Successfully printed!');
-            }
+		child.stderr.on('data', (data) => {
+			this._displayMessage('There was an error printing:', data.toString());
+		});
 
-            // if (this.options.deleteGenerated) {
-            //     this._cleanup(file);
-            // }
+		child.on('exit', (code, signal) => {
+			if (signal) {
+				this.log(`Exited with code ${code} and signal ${signal}`);
+			} else {
+				this._displayMessage(false, 'Successfully printed!');
+			}
 
-            end();
-        });
-    }
+			// if (this.options.deleteGenerated) {
+			//     this._cleanup(file);
+			// }
 
-    _displayMessage(head, content) {
-        if (head) {
-            this.log("\n");
-            this.log(head);
-        }
+			end();
+		});
+	}
 
-        this.log("\n");
-        this.log(content.split("\n").map(m => "\t" + m).join("\n"));
-        this.log("\n");
-    }
+	_displayMessage(head, content) {
+		if (head) {
+			this.log('\n');
+			this.log(head);
+		}
 
-    // _cleanup(file) {
-    //     fs.unlinkSync(file);
-    // }
+		this.log('\n');
+		this.log(content.split('\n').map((m) => '\t' + m).join('\n'));
+		this.log('\n');
+	}
 
-    printText(text, options = {}) {
-        return new Promise((resolve, reject) => {
-            const _opts = this._mergeOptions(this.options, options);
+	// _cleanup(file) {
+	//     fs.unlinkSync(file);
+	// }
 
-            const doc = this._getDocument(_opts);
-            this._createOutput(doc, _opts, resolve);
+	printText(text, options = {}) {
+		return new Promise((resolve, reject) => {
+			const _opts = this._mergeOptions(this.options, options);
 
-            doc.fontSize(_opts.fontSize);
-            doc.text(text, 0, 0);
-            doc.end();
-        });
-    }
+			const doc = this._getDocument(_opts);
+			this._createOutput(doc, _opts, resolve);
 
-    printImage(image, options = {}) {
-        return new Promise((resolve, reject) => {
-            const _opts = this._mergeOptions(this.options, options);
+			doc.fontSize(_opts.fontSize);
+			doc.text(text, 0, 0);
+			doc.end();
+		});
+	}
 
-            const doc = this._getDocument(_opts);
-            this._createOutput(doc, _opts, resolve);
+	printImage(image, options = {}) {
+		return new Promise((resolve, reject) => {
+			const _opts = this._mergeOptions(this.options, options);
 
-            doc.image(image, 0, 0, { width: _opts.width });
-            doc.end();
-        });
-    }
+			const doc = this._getDocument(_opts);
+			this._createOutput(doc, _opts, resolve);
 
-    printCustom(drawFunc, options = {}) {
-        return new Promise((resolve, reject) => {
-            const _opts = this._mergeOptions(this.options, options);
+			doc.image(image, 0, 0, { width: _opts.width });
+			doc.end();
+		});
+	}
 
-            const doc = this._getDocument(_opts);
-            this._createOutput(doc, _opts, resolve);
+	printCustom(drawFunc, options = {}) {
+		return new Promise((resolve, reject) => {
+			const _opts = this._mergeOptions(this.options, options);
 
-            drawFunc(doc, _opts.width, _opts.height);
+			const doc = this._getDocument(_opts);
+			this._createOutput(doc, _opts, resolve);
 
-            doc.end();
-        });
-    }
+			drawFunc(doc, _opts.width, _opts.height);
 
-    printFile(file, options = {}) {
-        return new Promise((resolve, reject) => {
-            const _opts = this._mergeOptions(this.options, options);
-            this._sendFile(file, _opts, resolve);
-        });
-    }
+			doc.end();
+		});
+	}
 
-    _getDocument({ width, height, margins }) {
-        return new PDFDocument({
-            size: [ width, height ],
-            margins: margins
-        });
-    }
+	printFile(file, options = {}) {
+		return new Promise((resolve, reject) => {
+			const _opts = this._mergeOptions(this.options, options);
+			this._sendFile(file, _opts, resolve);
+		});
+	}
 
-    log(msg) {
-        // Possible TODO - Allow other reporting method
-        console.log(msg);
-    }
+	_getDocument({ width, height, margins }) {
+		return new PDFDocument({
+			size: [ width, height ],
+			margins: margins
+		});
+	}
+
+	log(msg) {
+		// Possible TODO - Allow other reporting method
+		console.log(msg);
+	}
 }
 
 module.exports = CTLPrinter;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router(),
-	uuid = require('uuid');
+	uuid = require('uuid'),
+	printUtil = require('../public/js/print.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next){
@@ -15,9 +16,17 @@ router.post('/draw/save', function(req, res){
 			console.log(err);
 			return res.status(500).send(err);
 		}
-
-		res.send({ success: true, filename: filename });
+		printUtil.printPdf(filename);
+		res.json({ success: true, filename: filename });
 	});
+});
+
+router.post('/print', async (req, res) => {
+	try {
+	} catch (err) {
+		console.log(err);
+		res.json({ success: false });
+	}
 });
 
 module.exports = router;
